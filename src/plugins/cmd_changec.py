c34cf62752605b252ed54c0d2a9193d239b1ed8c
Jonas Grawe <shemneidev@gmail.com>
2016-09-29 00:00:04 +0200

    bugfix: changec will now only accept valid channel name syntax

--- src/plugins/cmd_changec.py
+++ src/plugins/cmd_changec.py
@@ -10,9 +10,12 @@ class IRCPlugin(master.Plugin):
 
     def cmd(self, line):
         channel = re.sub(self.get_regex(), "", line).strip()
-        if channel:
-            print("DEBUG: CHANGING CHANNELS")
-            self.connection.join_channel(channel.lower())
+        match = re.search(r"^([a-zA-Z0-9_]+)$", channel)
+        if match:
+            print(self.connection.Color.GREEN + "DEBUG: CHANGING CHANNELS" + self.connection.Color.RESET)
+            self.connection.join_channel(match.group().lower())
+        else:
+            print(self.connection.Color.BRIGHT_RED + "DEBUG: INVALID CHANNEL NAME" + self.connection.Color.RESET)
 
     def get_description(self):
         return "!changec [channel] - Switches to that channel (leaves current)"
